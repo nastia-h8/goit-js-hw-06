b8945ab482d6c9edee18cf0cc3c5ba6fd1932b09
Anastasiia Haliara <an.haliara@gmail.com>
2022-07-06 15:29:30 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,8 @@
 const categoriesRefs = document.querySelectorAll('.item');
 console.log("Number of categories:", categoriesRefs.length);
 
-
 const categoriesArr = [...categoriesRefs]
 categoriesArr.forEach(function (element){
-    const elementsTitle = element.querySelector('h2');
-    const elementsNumber = element.querySelectorAll('li');
-
-    console.log(`Category: ${elementsTitle.textContent}`);
-    console.log(`Elements: ${elementsNumber.length}`);
+    console.log(`Category: ${element.querySelector('h2').textContent}`);
+    console.log(`Elements: ${element.querySelectorAll('li').length}`);
 })
