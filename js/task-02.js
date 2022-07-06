d369f4841989f985566044dc44088298e0c9cd00
Anastasiia Haliara <an.haliara@gmail.com>
2022-07-06 16:01:17 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients')
+
+const newElements = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+})
+
+ingredientsListRef.append(...newElements)
